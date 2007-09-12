a2a19e5d76546cdfd2ae1e8236f9fb8104e49914
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2007-09-12 18:18:23 +0000

    Fixed NARPLUGIN-155

--- plugin/src/main/java/org/freehep/maven/nar/Fortran.java
+++ plugin/src/main/java/org/freehep/maven/nar/Fortran.java
@@ -5,11 +5,11 @@ package org.freehep.maven.nar;
  * Fortran compiler tag
  *
  * @author <a href="Mark.Donszelmann@slac.stanford.edu">Mark Donszelmann</a>
- * @version $Id: plugin/src/main/java/org/freehep/maven/nar/Fortran.java 631dc18040bb 2007/07/17 14:21:11 duns $
+ * @version $Id: plugin/src/main/java/org/freehep/maven/nar/Fortran.java 0c1f0fc112ac 2007/09/12 18:18:23 duns $
  */
 public class Fortran extends Compiler {
   
-	Fortran() {
+	public Fortran() {
 	}
 	
     public String getName() {
